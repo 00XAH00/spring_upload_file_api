4bc219e67962d6ac671a5111c0a9fada2f030411
00XAH00 <pxaxulin@gmail.com>
2023-05-03 13:12:27 +0300

    init project

---
+++ src/main/kotlin/com/xah/download/DownloadApplication.kt
@@ -0,0 +1,11 @@
+package com.xah.download
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DownloadApplication
+
+fun main(args: Array<String>) {
+    runApplication<DownloadApplication>(*args)
+}
---
+++ src/test/kotlin/com/xah/download/DownloadApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.xah.download
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DownloadApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
